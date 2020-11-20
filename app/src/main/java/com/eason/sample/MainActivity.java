c8d0d51247dc037b6bbbcb2cfa37e18e392e8907
Eason <d0938687689@gmail.com>
2020-11-20 23:10:33 +0800

    Button onClickListener

--- app/src/main/java/com/eason/sample/MainActivity.java
+++ app/src/main/java/com/eason/sample/MainActivity.java
@@ -2,13 +2,32 @@ package com.eason.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvResult;
+    EditText etName;
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvResult = findViewById(R.id.tvResult);
+        etName = findViewById(R.id.etName);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvResult.setText(etName.getText().toString());
+                tvResult.setTextColor(Color.BLUE);
+            }
+        });
     }
 }
