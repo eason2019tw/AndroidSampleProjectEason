5b7ba61b399101dd4e6c2238c74221e26a43133b
eason2019tw <eason.2019.tw@gmail.com>
2020-12-14 23:05:39 +0800

    sample for calc

--- app/src/main/java/com/eason/sample/CalcActivity.java
+++ app/src/main/java/com/eason/sample/CalcActivity.java
@@ -10,11 +10,17 @@ import android.widget.Toast;
 
 public class CalcActivity extends AppCompatActivity {
 
+    String temp_str = "";
+    String method = "";
+    int number1 = -1, number2 = -1;
+
+    TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
         TextView tvTitle = findViewById(R.id.tvTitle);
+        tvResult = findViewById(R.id.tvResult);
         if (getIntent()!=null){
             tvTitle.setText(getIntent().getStringExtra("title"));
         }
@@ -22,6 +28,50 @@ public class CalcActivity extends AppCompatActivity {
 
     public void calcClick(View view) {
         Button button = (Button) view;
-        Toast.makeText(CalcActivity.this, button.getText().toString(), Toast.LENGTH_SHORT).show();
+        String btn_text = button.getText().toString();
+
+        if (btn_text.equals("AC") || btn_text.equals("C")){
+            tvResult.setText("0");
+            initNumber();
+        }else if (btn_text.equals("=")){
+            if (method.equals("÷")){
+                temp_str = String.valueOf((number1 / number2));
+            }else if (method.equals("×")){
+                temp_str = String.valueOf((number1 * number2));
+            }else if (method.equals("－")){
+                temp_str = String.valueOf((number1 - number2));
+            }else if (method.equals("+")){
+                temp_str = String.valueOf((number1 + number2));
+            }
+            tvResult.setText(temp_str);
+            initNumber();
+        }else if (btn_text.equals("÷") || btn_text.equals("×") || btn_text.equals("－") || btn_text.equals("+")){
+            if (!tvResult.getText().toString().equals("0")){
+                method = btn_text;
+                number1 = Integer.parseInt(tvResult.getText().toString());
+                temp_str = "";
+            }
+        }else{
+            try {
+                temp_str+=btn_text;
+                if (method.equals("")){
+                    number1 = Integer.parseInt(temp_str);
+                }else{
+                    number2 = Integer.parseInt(temp_str);
+                }
+                tvResult.setText(temp_str);
+            }catch (NumberFormatException e){
+                Toast.makeText(CalcActivity.this, "not support: "+ btn_text, Toast.LENGTH_SHORT).show();
+                tvResult.setText("0");
+                initNumber();
+            }
+        }
+    }
+
+    private void initNumber(){
+        temp_str = "";
+        method = "";
+        number1 = -1;
+        number2 = -1;
     }
 }
